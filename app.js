815a5caac8053132a11448b5fbc19ecddbdb9f2f
Abd El Rahman Fouad <abdofouad.cs@gmail.com>
2022-08-23 21:32:59 +0200

    33

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    // console.log(req.url, req.method, req.headers);
     const url = req.url;
     if (url === '/') {
         res.write('<html>');
@@ -9,7 +9,14 @@ const server = http.createServer((req, res) => {
         res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
         res.write('</html>');
         return res.end();
-      }
+    }
+
+    if (url === '/message' && req.method === 'POST') {
+        fs.writeFileSync('message.txt', 'DUMMY');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>my first page</title></head>');
