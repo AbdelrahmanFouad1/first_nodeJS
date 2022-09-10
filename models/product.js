910d7fbefee78895fe04846d2aed4ca822721cad
Abd El Rahman Fouad <abdofouad.cs@gmail.com>
2022-09-10 13:04:34 +0200

    102

--- models/product.js
+++ models/product.js
@@ -24,7 +24,7 @@ module.exports = class Product {
         });
     }
 
-    static fetchAll() {
+    static fetchAll(callBack) {
         const p = path.join(
             path.dirname(require.main.filename),
             'data',
@@ -32,9 +32,9 @@ module.exports = class Product {
         );
         fs.readFile(p, (err, fileContent) => {
             if(err){
-                return [];
+                callBack([]);
             }
-            return JSON.parse(fileContent);
+            callBack(JSON.parse(fileContent));
         });
     }
 }
