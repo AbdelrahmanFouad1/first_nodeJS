1156e4d560b031580d45081ec079e94368a682b6
Abd El Rahman Fouad <abdofouad.cs@gmail.com>
2022-09-10 11:59:11 +0200

    98

--- app.js
+++ app.js
@@ -2,33 +2,24 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-// const expressHBS = require('express-handlebars');
+
 
 const app = express();
 
-// app.engine('hbs',
-//  expressHBS({
-//         layoutsDir: 'views/layouts/',
-//         defaultLayout: 'main_layout',
-//         extname: 'hbs'
-//  }));
 
-// app.set('view engine', 'pug');
-// app.set('view engine', 'hbs');
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopeRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended : false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopeRoutes);
 
 app.use((req, res, next) =>{
-//  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
     res.status(404).render('404', {pageTitle: 'Page Not Found', path: ''});
 });
 
